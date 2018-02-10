ea7d4214960d0be8d48eb8f39502c93e6de80389
Anıl Kılıç <anlklc@gmail.com>
2018-02-10 22:32:36 +0300

    Approved and rejected ones side by side. Shows latest 5.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
 var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://api.utopian.io/api/posts/?type=graphics&limit=50');
+var secondRequest = new XMLHttpRequest();
+
+ourRequest.open('GET', 'https://api.utopian.io/api/posts/?type=graphics&status=reviewed&limit=5');
 ourRequest.onload = function() {
-
   var obj = JSON.parse(ourRequest.responseText);
-
-  // why returns only 6 objects?
-  // - when it hits to an imageless object it stucks there.
 
   for (o of obj["results"]) {
     console.log(o);
@@ -16,14 +14,37 @@ ourRequest.onload = function() {
           <img class="first-image img-fluid img-thumbnail" src=" ${(o.json_metadata.hasOwnProperty("image")) ? o.json_metadata.image[0] : ""} "/>
         </div>
         <div class="col">
-          <a href="https://utopian.io${o.url}"> ${o.title}</a> 
+          <a href="https://utopian.io${o.url}"> ${o.title}</a>
           <p class="author">${o["author"]}</p>
-          <p class="moderator">${o["moderator"]} ${ (o["reviewed"]) ? "approved" : "rejected"}</p>
+          <p class="moderator">${ (o["reviewed"]) ? "approved" : "rejected"} by ${o["moderator"]} </p>
         </div>
       </div>
     </div>
     `
   }
-
 }
 ourRequest.send();
+
+secondRequest.open('GET', 'https://api.utopian.io/api/posts/?type=graphics&status=flagged&limit=5')
+secondRequest.onload = function() {
+  var obj = JSON.parse(secondRequest.responseText);
+
+  for (o of obj["results"]) {
+    console.log(o);
+    document.getElementById("sapp").innerHTML += `
+    <div class="container new-post">
+      <div class="row">
+        <div class="col">
+          <img class="first-image img-fluid img-thumbnail" src=" ${(o.json_metadata.hasOwnProperty("image")) ? o.json_metadata.image[0] : ""} "/>
+        </div>
+        <div class="col">
+          <a href="https://utopian.io${o.url}"> ${o.title}</a>
+          <p class="author">${o["author"]}</p>
+          <p class="moderator">${ (o["reviewed"]) ? "approved" : "rejected"} by ${o["moderator"]}</p>
+        </div>
+      </div>
+    </div>
+    `
+  }
+}
+secondRequest.send();
