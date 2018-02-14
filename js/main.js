c7a19cd36f54b7fcde1f7642fe80d2ec7821e5f1
Anıl Kılıç <anlklc@gmail.com>
2018-02-14 23:40:12 +0300

    Cleaner code with option interactions to select type/status/limit

--- js/main.js
+++ js/main.js
@@ -1,50 +1,52 @@
-var ourRequest = new XMLHttpRequest();
-var secondRequest = new XMLHttpRequest();
+function myFunc() {
+  var ourRequest = new XMLHttpRequest();
 
-ourRequest.open('GET', 'https://api.utopian.io/api/posts/?type=graphics&status=reviewed&limit=5');
-ourRequest.onload = function() {
-  var obj = JSON.parse(ourRequest.responseText);
+  var type = document.getElementById("type").value;
+  var status = document.getElementById("status").value;
+  var limit = document.getElementById("limit").value;
 
-  for (o of obj["results"]) {
-    console.log(o);
-    document.getElementById("app").innerHTML += `
-    <div class="container new-post">
-      <div class="row">
-        <div class="col">
-          <img class="first-image img-fluid img-thumbnail" src=" ${(o.json_metadata.hasOwnProperty("image")) ? o.json_metadata.image[0] : ""} "/>
-        </div>
-        <div class="col">
-          <a href="https://utopian.io${o.url}"> ${o.title}</a>
-          <p class="author">${o["author"]}</p>
-          <p class="moderator">${ (o["reviewed"]) ? "approved" : "rejected"} by ${o["moderator"]} </p>
-        </div>
-      </div>
-    </div>
-    `
-  }
-}
-ourRequest.send();
 
-secondRequest.open('GET', 'https://api.utopian.io/api/posts/?type=graphics&status=flagged&limit=5')
-secondRequest.onload = function() {
-  var obj = JSON.parse(secondRequest.responseText);
+  ourRequest.open('GET', `https://api.utopian.io/api/posts/?type=${type}&status=${status}&limit=${parseInt(limit)}`);
+  console.log(`https://api.utopian.io/api/posts/?type=${type}&status=${status}&limit=${parseInt(limit)}`)
+  ourRequest.onload = function() {
+    var obj = JSON.parse(ourRequest.responseText);
 
-  for (o of obj["results"]) {
-    console.log(o);
-    document.getElementById("sapp").innerHTML += `
-    <div class="container new-post">
-      <div class="row">
-        <div class="col">
-          <img class="first-image img-fluid img-thumbnail" src=" ${(o.json_metadata.hasOwnProperty("image")) ? o.json_metadata.image[0] : ""} "/>
-        </div>
-        <div class="col">
-          <a href="https://utopian.io${o.url}"> ${o.title}</a>
-          <p class="author">${o["author"]}</p>
-          <p class="moderator">${ (o["reviewed"]) ? "approved" : "rejected"} by ${o["moderator"]}</p>
+    for (o of obj["results"]) {
+      console.log(o);
+      document.getElementById("app").innerHTML += `
+      <div class="container new-post">
+        <div class="row">
+          <div class="col">
+            <img class="first-image img-fluid img-thumbnail" src=" ${(o.json_metadata.hasOwnProperty("image")) ? o.json_metadata.image[0] : ""} "/>
+          </div>
+          <div class="col">
+            <a href="https://utopian.io${o.url}"> ${o.title}</a>
+            <p class="author">${o["author"]}</p>
+            <p class="moderator">${ (o["reviewed"]) ? "approved" : "rejected"} by ${o["moderator"]} </p>
+            <button id="showInPage" type="button" onclick="showInPage()">Show *WIP</button>
+          </div>
         </div>
       </div>
-    </div>
-    `
+      `
+
+    }
+
+    // function showInPage() {
+    //   var converter = new showdown.Converter();
+    //   var md = `${o.body}`;
+    //   var html = converter.makeHtml(md);
+    //
+    //   document.getElementById("sapp").innerHTML += `
+    //   <div class="container new-post">
+    //     <div class="row">
+    //       <div class="col">
+    //         ${html}
+    //       </div>
+    //     </div>
+    //   </div>
+    //   `
+    // }
+
   }
-}
-secondRequest.send();
+  ourRequest.send();
+};
